ee1c830e370a452f18119512610f4f95c1695911
pcmagas <pc_magas@yahoo.gr>
2015-10-17 20:52:28 +0300

    Sending html

--- application/models/Offers_model.php
+++ application/models/Offers_model.php
@@ -8,9 +8,9 @@ class Offers_model extends CI_Model
 		$this->load->database();
 	}
 	
-	public function get($keyword=null,$id=null,$price=null)
+	public function get($keyword=null,$id=null,$price=null,$place_id=null)
 	{
-		if(empty($keyword) && empty($id)) return array();
+		if(empty($keyword) && empty($id) && empty($price) && empty($place_id)) return array();
 		
 		$this->db->select('`Offers`.`id`,
 						   `Offers`.`name` as `offer_name` ,
@@ -25,10 +25,12 @@ class Offers_model extends CI_Model
 				 ->join('`Business`','`Business`.`id`=`Offers`.`business_id`')
 				 ->join('`Keywords`','`Keywords`.`offer_id`=`Offers`.`id`')
 				 ->join('`Places`','`Places`.`id`=`Business`.`place_id`')
-				 ->like('`Keywords`.`name`',$keyword)
 				 ->order_by('`Offers`.`price`','ASC');
-				 
+
+		if(!empty($keyword))$this->db->like('`Keywords`.`name`',$keyword);				 
 		if(!empty($price)) $this->db->where('`Offers`.`price`<=',$price);
+		if(!empty($id)) $this->db->where('`Offers`.`id`',$id);
+		if(!empty($pplade_id)) $this->db->where('`Places`.`id`',$place_id);
 		
 		$q=$this->db->get();
 
