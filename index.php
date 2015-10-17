cb5acc7390136ef8b3f5ff70cd95126edb449c02
Dimitrios Desillas <ddesillas@yahoo.gr>
2015-10-18 00:28:49 +0300

    All application

--- index.php
+++ index.php
@@ -1,292 +1,148 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	http://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
+//include('db.php');
+?>
+<html>
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+<head>
+	<meta charset="UTF8">
+	<title>Dream Planner</title>
+<script src="//code.jquery.com/jquery1.11.3.min.js"></script>
+<script stc="http://knockoutjs.com/downloads/knockout3.3.0.js"></script>
+<script>
 
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
-switch (ENVIRONMENT)
+var url="../backend/index.php/offers/get";
+
+var get_offers=function(keyword,price,success_cb,error_cb,alwayscb)
 {
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+	var postdata={'keyword':keyword};
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+	if(typeof price !=='undeifned' ) postdata['price'];
+	$.get(url,postdata)
+	.done(function(data)
+	{
+		data=data.data;
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+		if(typeof success_cb==='function') success_cb(data);
+	})
+	.always(function(data)
+	{
+		if(typeof alwayscb==='fuunction') alwayscb(data);
+	})
+	.error(function(data)
+	{
+		if(typeof error_cb === 'function') error_cb(data);
+	});
+};
+
+function Offer(data)
+{
+	var self=this;
+	self.order_name=ko.observable(data.offer_name);
+	self.business=ko.observable(data.business_name);
+	self.price=ko.observable(data.price);
+	self.description=ko.observable(data.description);
+	self.place_id=ko.observable(parseInt(data.place_id));
+	self.place_name=ko.observable(parseInt(data.place_name));
+	self.g_long=ko.observable(data['long']);
+	self.g_lat=ko.observable(data['lat']);
+};
+
+function IndexViewmodel()
+{
+	var self=this;
+
+	self.searchTerm=ko.observable();
+	self.offers=ko.observableArray();
+
+	self.searchTerm.subscribe(function(data)
+	{
+		get_offers(data,null,
+		function(data)
+		{
+			var offers=[];
+			ko.utils.arrayForEach(data,function(d)
+			{
+				var last_offer=new Offer(d);
+				offers.push(last_offer);
+			});
+			self.offers(offers);
+		},
+		null,
+		function(data)
+		{
+
+		});
+	});
+
+	self.setSearchTerm=function(data)
+	{
+		self.searchTerm(data.order_name());
+	};
 }
 
-/*
- *---------------------------------------------------------------
- * SYSTEM FOLDER NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same directory
- * as this file.
- */
-	$system_path = 'system';
+$(document).ready(function()
+{
+	var indexVm=new IndexViewmodel();
 
-/*
- *---------------------------------------------------------------
- * APPLICATION FOLDER NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * folder than the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server. If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- */
-	$application_folder = 'application';
+	ko.applyBindings(indexVm);
+});
+</script>
 
-/*
- *---------------------------------------------------------------
- * VIEW FOLDER NAME
- *---------------------------------------------------------------
- *
- * If you want to move the view folder out of the application
- * folder set the path to the folder here. The folder can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application folder. If you
- * do move this, use the full server path to this folder.
- *
- * NO TRAILING SLASH!
- */
-	$view_folder = '';
+</head>
+<body style="background:url('back.jpg') repeat;" >
 
+<div style="width:1100px; margin:auto; height:600px;" >
 
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-	// The directory name, relative to the "controllers" folder.  Leave blank
-	// if your controller is not in a sub-folder within the "controllers" folder
-	// $routing['directory'] = '';
+					<?php //if(!isset($_GET['s']))
+					      //{
+					?>
+							<div style="margin:auto; width:260px; margintop:10%;" >
+					<a href="index.php" ><img src="logo.png" /></a>
 
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
+								</div>
+							<form method="GET" action="index.php" style=" width:500px; margin:auto; margintop:30px;" >
+							<input name="s" databind="value:offers,valueUpdate:'keyup'" placeholder="What do you want to do?" type="text" style="height:35px; outline:none; padding: 0px 0px 0px 10px; width:400px;"  />
+							<button style="height:35px; width:80px; outline:none; marginleft:10px; background:#fff; border:none; cursor:pointer;" >Search</button>
+					</form>
+					<ul databind="foreach:offers">
+						<li data-bind="click:$parent."><span data-bind="text:name"></span></li>
+					</ul>
+					<?php //}else { ?>
+						<div style="width:1100px; height:1100px; margin:auto; background:#fff; position:relative;" >
+						<h3 style="textalign:center;" >Αποτελέσματα: </h3>
 
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
+						<div style="width:530px; position:relative; margin:auto; " >
+<?php //if(isset($_GET['s'])){ $name=$_GET['s']; $query = mysql_query("SELECT name, description, business_id, price, id FROM `offers` WHERE name='$name' ");
+  //while($row=mysql_fetch_array($query)) { $desc=$row['description']; $price=$row['price'];  $id=$row['id']; ?>
+						<div style="width:530px; boxshadow: 0px 0px 0px 1px #000; borderradius:2px; margintop:10px; marginbottom:10px; height:220px; float:left; background:#fff;" >
+								<h2 style="padding: 10px 0px 0px 10px;" ><?php //echo $row['name']; ?></h2>
+							<div style="width:400px; float:left; position:relative; height:130px;" >
+								<img src="image.jpg" style="width:130px; marginleft:10px; height:130px; float:left;" />
+								<div style="position:relative; float:left; position:relative;" >
+										<p style="marginleft:10px;">Όν. Επιχ.: <?php //$business_id=$row['business_id']; $query1 = mysql_query("SELECT name, place_id FROM `business` WHERE id='$business_id' ");
+//  while($row=mysql_fetch_array($query1)) { echo $bus_name=$row['name']; $place_id=$row['place_id'];  } ?></p>
+  									<p style="marginleft:10px;" >Περιοχή: <?php //$query2 = mysql_query("SELECT name FROM `places` WHERE id='$place_id' ");
+  //while($row=mysql_fetch_array($query2)) { echo $row['name']; } ?></p>
+										<p style="marginleft:10px;">Περιγραφή: <?php echo $desc; ?></p>
 
 
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
+								</div>
+							</div>
+							<a href="offers.php?id=<?php echo $id; ?>" ><div style="width:120px; cursor:pointer; background:#00FF00; float:left;  position:relative; height:130px;" >
+
+								<img src="tick.png" style="height:30px; margintop:50px; marginleft:40px;"/>
+							</div></a>
+						</div>
+						<?php //} } ?>
+
+ 						</div>
+ 							</div>
+					<?php //} ?>
+				</div>
+
+
+</body>
 
 
 
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.'/';
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = rtrim($system_path, '/').'/';
-	}
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// Path to the system folder
-	define('BASEPATH', str_replace('\\', '/', $system_path));
-
-	// Path to the front controller (this file)
-	define('FCPATH', dirname(__FILE__).'/');
-
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-
-		define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-		{
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-			exit(3); // EXIT_CONFIG
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
-	}
-
-	// The path to the "views" folder
-	if ( ! is_dir($view_folder))
-	{
-		if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-		{
-			$view_folder = APPPATH.$view_folder;
-		}
-		elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-		{
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-			exit(3); // EXIT_CONFIG
-		}
-		else
-		{
-			$view_folder = APPPATH.'views';
-		}
-	}
-
-	if (($_temp = realpath($view_folder)) !== FALSE)
-	{
-		$view_folder = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		$view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
-	}
-
-	define('VIEWPATH', $view_folder);
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
- */
-require_once BASEPATH.'core/CodeIgniter.php';
+</html>
