a520024e04dc4eb80984837c1a4c51bc31f69a9e
pcmagas <pc_magas@yahoo.gr>
2015-10-18 11:52:56 +0300

    Show Offers

--- backend/application/views/offers_page.php
+++ backend/application/views/offers_page.php
@@ -1,115 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-$this->load->helper('url');
 
-$this->load->view('page_header.php')
+foreach($data as $d)
+{
 ?>
-
-	<div style="width:800px; background:#fff; position:relative; margin:auto; height:1700px;" >
-	<h2 style="text-align:center;" >Offer Title</h2>
-	<div style="width:100%; height:200px;" >
-			<img src=" " style="width:230px; height:200px; margin-left:20px; float:left;" />
-			<div style="float:left; " >
-			<p style="margin-left:10px;" >Περιοχή: 
-			</p>
-			<p style="margin-left:10px;" >Περιγραφή:</p>
+<div style="width:530px; box-shadow: 0px 0px 0px 1px #000; border-radius:2px; margin-top:10px; margin-bottom:10px; height:220px; float:left; background:#fff;" >
+	<h2 style="padding: 10px 0px 0px 10px;" ><?=$d['offer_name']?></h2>
+	<div style="width:400px; float:left; position:relative; height:130px;" >
+			<img src="image.jpg" style="width:130px; margin-left:10px; height:130px; float:left;" />
+				<div style="position:relative; float:left; position:relative;" >
+					<p style="margin-left:10px;">Όν. Επιχ.:<?=$d['business_name']?>  </p>
+  					<p style="margin-left:10px;" >Περιοχή: <?=$d['place_name']?></p>
+					<p style="margin-left:10px;">Περιγραφή: <?=$d['description']?></p>								
+				</div>
 	</div>
-	</div>
-	<div style="width:100%; height:200px;" > 
-			<h2>&nbsp;&nbsp;&nbsp;Διαμονή: <select><option>Μονόκλινο</option><option>Δίκλινο</option><option>Τρικλινο</option></select></h2>
-			<div style="width:100%;" >
-
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-			</div>
-	</div>
-		<div style="width:100%; height:200px;" > 
-			<h2>&nbsp;&nbsp;&nbsp;Φαγητό: <select><option>Fast Food</option><option>Εστιατόριο</option><option>Ταβέρνα</option></select></h2>
-			<div style="width:100%;" >
-
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-			</div>
-	</div>
-	<div style="width:100%; height:200px;" > 
-			<h2>&nbsp;&nbsp;&nbsp;Πολιτισμός: <select><option>Αξιοθέατα</option><option>Μουσεία</option><option>Αρχαιολογική Χώροι</option><option>Παραδοσιακές Εκλοδηλώσεις</option></select></h2>
-			<div style="width:100%;" >
-
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-			</div>
-	</div>
-		<div style="width:100%; height:200px;" > 
-			<h2>&nbsp;&nbsp;&nbsp;Ψυχαγωγία: <select><option>Κινηματογράφοι</option><option>Θέατρο</option><option>Πολυχοροι</option></select></h2>
-			<div style="width:100%;" >
-
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-			</div>
-	</div>
-		<div style="width:100%; height:200px;" > 
-			<h2>&nbsp;&nbsp;&nbsp;Nightlife: <select><option>Clubs</option><option>Bars</option><option>Μουσικές Σκηνές</option></select></h2>
-			<div style="width:100%;" >
-
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-				<div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
-
-				</div>
-			</div>
-	</div>
-	<div style="width:300px; height:200px; margin-top:30px; margin-left:auto; " >
-				<h3>Τελική Τιμή:</h3>
-
-				<button style="width:300px; height:40px;" >Κράτηση</button>
-	</div>
-
+	<a href="offers.php?id=?" >
+		<div style="width:120px; cursor:pointer; background:#00FF00; float:left;  position:relative; height:130px;" >							
+			<img src="tick.png" style="height:30px; margin-top:50px; margin-left:40px;"/>
+		</div>
+	</a>
+</div>
 <?php
-	$this->load->view('page_footer');
+}
 ?>
