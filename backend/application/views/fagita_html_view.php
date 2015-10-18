a732814fee7025de07870c49b303394748964365
pcmagas <pc_magas@yahoo.gr>
2015-10-18 14:20:19 +0300

    Show Entertnainment

--- backend/application/views/fagita_html_view.php
+++ backend/application/views/fagita_html_view.php
@@ -2,14 +2,16 @@
 
 <div style="width:160px; height:150px; border:2px solid; float:left; margin-left:15px; margin-right:15px;" >
 
-<?php foreach($data as $d)
-	  {
+<?php
+ 
+	foreach($data as $d)
+	{
 	  	foreach($d as $dd)
 		{
 ?>
 			<p><?=$dd?></p>
 <?php
 		}	  	
-	  }
+	}
 ?>
 </div>
